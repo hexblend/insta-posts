9b1bdb95d708e7d321627fc60afb440af464d23c
vladbbr <vladbbr1@gmail.com>
2020-04-14 13:08:29 +0100

    Error handling

--- fullstack_blog/server/index.js
+++ fullstack_blog/server/index.js
@@ -13,9 +13,22 @@ mongoose
 	.then(() => console.log('DB Connected'))
 	.catch((error) => console.log(error));
 
-// Middlewares
+// Routes
 const postsRoutes = require('./routes/posts');
 app.use('/api/posts', postsRoutes);
+
+// 404 Catcher
+app.use((req, res, next) => {
+	const error = new Error('URL Not Found');
+	error.status = 404;
+	next(error);
+});
+
+// Error Handling
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.send({ message: `Error! ${error.message}`, error: error });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
