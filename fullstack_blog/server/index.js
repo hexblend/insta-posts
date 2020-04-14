8a286c6b3fdc406b71535ff059a30f10dc693aa4
vladbbr <vladbbr1@gmail.com>
2020-04-14 12:51:46 +0100

    FS Blog DB Connection established

--- fullstack_blog/server/index.js
+++ fullstack_blog/server/index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+
+// DB Connection
+mongoose
+	.connect(process.env.DB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('DB Connected'))
+	.catch((error) => console.log(error));
+
+// Middlewares
 app.get('/', function (req, res) {
 	res.send('hello world');
 });
