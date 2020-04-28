3fad3793d84aa56df5c51b4f8ba96a865f061085
vladbbr <vladbbr1@gmail.com>
2020-04-28 19:41:26 +0100

    Added transition icons

---
+++ transition_icons/script.js
@@ -0,0 +1,25 @@
+const button = document.querySelector('.btn');
+const icon = document.querySelector('.icon');
+
+icon.style.transition = '300ms ease-in-out';
+let open = false;
+
+button.addEventListener('click', () => {
+	if (!open) {
+		// Change to X
+		icon.style.transform = 'rotate(-90deg)';
+		setTimeout(
+			() => icon.setAttribute('class', 'fas fa-times icon'),
+			120
+		);
+		open = true;
+	} else {
+		// Change back to ▼
+		icon.style.transform = 'rotate(0)';
+		setTimeout(
+			() => icon.setAttribute('class', 'fas fa-angle-down icon'),
+			120
+		);
+		open = false;
+	}
+});
